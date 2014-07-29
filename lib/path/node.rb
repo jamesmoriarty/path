5a66bb4b13543c36abd2a783cb435e927ff7c392
James Moriarty <jamespaulmoriarty@gmail.com>
2014-07-29 20:06:07 +1000

    refinement

--- lib/path/node.rb
+++ lib/path/node.rb
@@ -10,18 +10,18 @@ class Node
     open = Array.new
     open.push(start)
 
-    distance = Hash.new
-    distance[start] = 0
+    distances = Hash.new
+    distances[start] = 0
 
     while(current = open.shift)
       current.neighbors.each do |neighbor|
-        unless distance[neighbor]
-          distance[neighbor] = distance[current] + 1
+        unless distances[neighbor]
+          distances[neighbor] = distances[current] + 1
           open.push(neighbor)
         end
       end
     end
 
-    return distance
+    return distances
   end
 end
