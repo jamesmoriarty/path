c687ad734897e5c552a3916fcfc40794718eac3b
James Moriarty <jamespaulmoriarty@gmail.com>
2014-08-11 22:50:14 +1000

    refinement: remove heuristic factor proc crap.

--- lib/path/node.rb
+++ lib/path/node.rb
@@ -3,25 +3,25 @@ module Path
     attr_accessor :data, :neighbors
 
     def initialize(data: data, neighbors: [])
-      @data    = data
+      @data      = data
       @neighbors = neighbors
     end
 
-    def self.df_search(to, from, &heuristic)
+    def self.df_search(from, to, heuristic)
       priority = Array.new
       priority.push(to)
 
       steps = Hash.new
       steps[to] = 0
 
-      while(current = priority.shift)
+      while(current = priority.shift) do
         break if current.neighbors.include?(from)
 
         current.neighbors.each do |neighbor|
           unless steps[neighbor]
             steps[neighbor] = steps[current] + 1
             priority.push(neighbor)
-            priority.sort_by!(&heuristic.call(to))
+            priority.sort_by! { |to| heuristic.call(to, from) }
           end
         end
       end
@@ -29,14 +29,14 @@ module Path
       return steps
     end
 
-    def self.bf_search(to, from)
+    def self.bf_search(from, to)
       frontier = Array.new
       frontier.push(to)
 
       steps = Hash.new
       steps[to] = 0
 
-      while(current = frontier.shift)
+      while(current = frontier.shift) do
         break if current.neighbors.include?(from)
 
         current.neighbors.each do |neighbor|
